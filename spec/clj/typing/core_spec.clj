162c96ecf563b184938a67ce947a44d7e1ad1aa0
Logan Buckley <lbuckley@opinionlab.com>
2016-04-14 09:23:22 -0400

    Fix source paths, whitespace, reverse test conditions

--- spec/clj/typing/core_spec.clj
+++ spec/clj/typing/core_spec.clj
@@ -4,11 +4,11 @@
             [typing.core :refer :all]))
 
 (describe "Typing API"
-          (context "GET /"
-                   (it "starts with an empty message"
-                       (should= (app (mock/request :get "/"))
-                                  {:status 200
-                                   :headers {"Content-Type" "application/json"}
-                                   :body {}}))))
+  (context "GET /"
+    (it "starts with an empty message"
+      (should= {:status 200
+                :headers {"Content-Type" "application/json"}
+                :body {}}
+               (app (mock/request :get "/"))))))
 
 (run-specs)
